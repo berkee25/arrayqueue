4a19616e1880b23362824a1823574d21d7f43c57
berkee25 <berkeozdemir25@gmail.com>
2025-11-04 23:12:13 +0300

    Update myq.java

--- src/myq.java
+++ src/myq.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class myq {
+    int front , rear ,size;
+    int capacity;
+    int [] array;
+
+    public myq (int capacity){
+        this.capacity = capacity;
+        front = size = 0;
+        rear = capacity -1 ;
+        array = new int[capacity];
+    }
+    boolean isEmpty(){
+        return (size == 0);
+    }
+    boolean isFull(){
+        return (size == capacity);
+    }
+
+    void enqueue(int value){
+        if (isFull())
+            return ;
+        rear = (rear + 1) % capacity; // to make circular queue
+        array[rear] = value;
+        size++;
+    }
+    int dequeue(){
+        if (isEmpty())
+            return -1;
+        int value = array[front];
+        front = (front + 1) % capacity;
+        size --;
+        return value;
+    }
+    int front(){
+        if(isEmpty())
+            return -1;
+        return array[front];
+    }
+    int rear(){
+        if(isEmpty())
+            return -1;
+        return array[rear];
+    }
+    void print(){
+        for(int front = 0; front < size; front++){
+            System.out.print(array[front] + " ");
+        }
+        System.out.println();
+    }
 }
